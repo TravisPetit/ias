90433bc4b263a05fe0f7ade74c4731d00a78fc49
travis <travis@debian-x220>
2020-03-16 10:38:32 +0100

    Add rudimentary support for select in afug1

--- aufgabe1/tcpclient.py
+++ aufgabe1/tcpclient.py
@@ -1,4 +1,4 @@
-import socket
+import socket, select, sys
 
 serverName = socket.gethostname()
 serverPort = 12000
@@ -12,11 +12,29 @@ clientSocket.connect((serverName, serverPort))
 uname = input("enter a Username: ")
 clientSocket.send(uname.encode())
 
-while True:	
-	message = input("{}: ".format(uname))
-	clientSocket.send(message.encode())
+things_we_are_going_to_read = [clientSocket, sys.stdin]
+things_we_are_going_to_write = [clientSocket]
+things_we_might_error_on = [clientSocket, sys.stdin]
 
-	reply = clientSocket.recv(1024)
-	print("From Server: {}".format(reply.decode()))
+partner_uname = "SERVER"
+
+while True:	
+	read_input, _, exceptions = select.select(things_we_are_going_to_read, things_we_are_going_to_write, things_we_might_error_on)
+
+	for notified_input in read_input:
+		if notified_input == sys.stdin:
+			toSend = sys.stdin.readline().strip()
+			clientSocket.send(toSend.encode())
+
+		if notified_input == clientSocket:
+			recieved_message = clientSocket.recv(1024).decode()
+			print("{}: {}".format(partner_uname, recieved_message))
+
+
+	#message = input("{}: ".format(uname))
+	#clientSocket.send(message.encode())
+
+	#reply = clientSocket.recv(1024)
+	#print("From Server: {}".format(reply.decode()))
 
 clientSocket.close()
--- aufgabe1/tcpserver.py
+++ aufgabe1/tcpserver.py
@@ -1,4 +1,17 @@
-import socket
+import socket, select, sys
+
+#def recieve_message(client_scoket):
+#	try:
+#		message_header = client_socket.recv(10) #10 is the header length
+#
+#		if no len(message_header):
+#			return False
+#
+#		message_length = int(message_header.decode().strip())
+#		return {"header": message_header, "data": client_socket.recv(len(message_header))}
+#
+#	except:
+#		return False
 
 serverPort = 12000
 
@@ -13,17 +26,29 @@ serverSocket.listen(1) # set the maximum number of queued connections requests t
 my_uname = input("Enter a username: ")
 
 connectionSocket, addr = serverSocket.accept() # create the third socket, the tunnel for connecting with the client
-
 print("Someboy connected!")
-
 partner_uname = connectionSocket.recv(1024).decode()
-
 print("It's {}".format(partner_uname))
 
+things_we_are_going_to_read = [connectionSocket, sys.stdin]
+things_we_are_going_to_write = [connectionSocket]
+things_we_might_error_on = [connectionSocket, sys.stdin]
+
 while True:
-	message = connectionSocket.recv(1024).decode()
-	print("{}: {}".format(partner_uname, message))
-	reply = input("{}: ".format(my_uname))
-	connectionSocket.send(reply.encode())
+	read_input, _, exceptions = select.select(things_we_are_going_to_read, things_we_are_going_to_write, things_we_might_error_on)
+
+	for notified_input in read_input:
+		if notified_input == sys.stdin:
+			toSend = sys.stdin.readline().strip().encode()
+			connectionSocket.send(toSend)
+
+		if notified_input == connectionSocket:
+			recieved_message = connectionSocket.recv(1024).decode()
+			print("{}: {}".format(partner_uname, recieved_message))
+
+	#message = connectionSocket.recv(1024).decode()
+	#print("{}: {}".format(partner_uname, message))
+	#reply = input("{}: ".format(my_uname))
+	#connectionSocket.send(reply.encode())
 
 connectionSocket.close()
