bbc6fffc313f3b81baf332f50d8f4fe2390602ec
travis <travis@debian-x220>
2020-04-01 14:03:28 +0200

    WIP

--- aufgabe3/reciever.py
+++ aufgabe3/reciever.py
@@ -1,47 +1,81 @@
 import socket
+import select
 import struct
 import sys
 
-multicast_group = '224.3.29.71'
-server_address = ('', 10000)
+number_servers = int(input("Enter the number of servers desired: "))
 
-# Create the socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+groups = []
+address_sockets = []
 
-# Bind to the server address
-sock.bind(server_address)
-
-
-# Tell the operating system to add the socket to the multicast group
-# on all interfaces:
-
-# converts the group address into binary
-group_in_byte_format = socket.inet_aton(multicast_group) 
-
-# INADDR_ANY is a constant that is equal to 0. It serves as a flag that tells the bind() function to bind a socket to
-# all possible interfaces
-
-# group together the group and INADDR_ANY constant and pack them together into a binary number
-# 4sl means that what we are packing are 4 strings and and unsigled long digit:
-# the four strings in this case are (I think) 224, 3, 29 and 71 and the unsigned long is just 0
-mreq = struct.pack('4sL', group_in_byte_format, socket.INADDR_ANY)
+class Address:
+	def __init__(self, multicast_ip, port):
+		self.ip = multicast_ip
+		self.port = ("", port)
 
 
-# the first parameter tells the kernel at what level to handle this option,
-# we choose  IPPROTO_IP to handle it at the IP level, this is always the case for multicasting
-# the second parameter is the option we are setting: we want to add the socket to the multicast group so we
-# set it to IP_ADD_MEMBERSHIP
-# the third parameter is, in this case, what we want to add, why is myreq
-sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+for i in range(0, number_servers):
+	groups.append(Address("224.3.29.{}".format(70+i), 10000 + i))
+	print(groups[i].ip)
+	print(groups[i].port)
 
 
-# Receive/respond loop
+for addr in groups:
+	# Create the socket
+	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+	# Bind to the server address
+	#sock.bind(server_address)
+	sock.bind(addr.port)
+
+
+	# Tell the operating system to add the socket to the multicast group
+	# on all interfaces:
+
+	# converts the group address into binary
+	#group_in_byte_format = socket.inet_aton(group) 
+	group_in_byte_format = socket.inet_aton(addr.ip)
+
+	# INADDR_ANY is a constant that is equal to 0. It serves as a flag that tells the bind() function to bind a socket to
+	# all possible interfaces
+
+	# group together the group and INADDR_ANY constant and pack them together into a binary number
+	# 4sl means that what we are packing are 4 strings and and unsigled long digit:
+	# the four strings in this case are (I think) 224, 3, 29 and 71 and the unsigned long is just 0
+	mreq = struct.pack('4sL', group_in_byte_format, socket.INADDR_ANY)
+
+
+	# the first parameter tells the kernel at what level to handle this option,
+	# we choose  IPPROTO_IP to handle it at the IP level, this is always the case for multicasting
+	# the second parameter is the option we are setting: we want to add the socket to the multicast group so we
+	# set it to IP_ADD_MEMBERSHIP
+	# the third parameter is, in this case, what we want to add, why is myreq
+	sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+
+	sock.settimeout(1/number_servers)
+
+	address_sockets.append((addr, sock))
+
+
+
+
 while True:
-    print("waiting to receive message")
-    data, address = sock.recvfrom(1024)
-    
-    print('received {} bytes from {}'.format(len(data), address))
-    print(data)
-
-    print('sending acknowledgement to {}'.format(address))
-    sock.sendto('ack'.encode(), address)
+#	for address_sock in address_sockets:
+#		addr = address_sock[0]
+#		sock = address_sock[1]
+#		try:
+#			data, _ = sock.recvfrom(1024)
+#			print(data.decode())
+#			sock.sendto("ack".encode(), (addr.ip, addr.port[1]))
+#		except socket.timeout:
+#			continue
+	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	s.bind(("", 1234))
+	client_uname, client_addr = s.recvfrom(2048) #recieve clinet uname
+	client_uname = client_uname.decode()
+	print("{} joined the server!".format(client_uname))
+	s.sendto("There are {} servers avaliable, reply with a server index".format(number_servers).encode(), client_addr)
+	client_choice, _ = s.recvfrom(2048)
+	print("{} server choice: {}".format(client_uname, client_choice.decode()))
+	s.sendto(groups[int(client_choice.decode())].ip.encode(), client_addr) # send ip
+	s.sendto(str(groups[int(client_choice.decode())].port[1]).encode(), client_addr) # send port
--- aufgabe3/sender.py
+++ aufgabe3/sender.py
@@ -1,15 +1,46 @@
 import socket
 import struct
+import select
+import sys
+
+blue = '\033[94m'
+green = '\033[92m'
+endcolor = '\033[0m'
+
+unicast_addr = input("Enter server addr: ")
+uname = input("Enter username: ")
+unicast_port = 1234
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+print("Connecting to server ...")
+
+s.sendto(uname.encode(), (unicast_addr, unicast_port))
+mssg, server = s.recvfrom(2048)
+print(blue + "### MSSG FROM SERVER ###")
+print(mssg.decode() + endcolor)
+
+server_nr = input("Enter server nr: ")
+s.sendto(server_nr.encode(), server)
+server_ip, _ = s.recvfrom(2048) # obtain ip
+server_ip = server_ip.decode()
+server_port, _ = s.recvfrom(2048) # obtain port
+server_port = int(server_port.decode())
+
+s.close()
+print("Joining Multicast chat room on addr {} and port {} ...".format(server_ip, server_port))
+
+
 
 message = 'very important data'
-multicast_group = ('224.3.29.71', 10000)
+#multicast_group = ('224.3.29.70', 10000)
+multicast_group = (server_ip, server_port)
 
 # Create the datagram socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # Set a timeout so the socket does not block indefinitely when trying
 # to receive data.
-sock.settimeout(0.2)
+sock.settimeout(5)
 
 # Set the time-to-live for messages to 1 so they do not go past the
 # local network segment. Again, in binary:
@@ -19,20 +50,23 @@ ttl = struct.pack('b', 1)
 sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
 
 try:
-    # Send data to the multicast group
-    print ('sending {}'.format(message))
-    sent = sock.sendto(message.encode(), multicast_group)
-
-    # Look for responses from all recipients
-    while True:
-        print('waiting to receive')
-        try:
-            data, server = sock.recvfrom(16)
-        except socket.timeout:
-            print('timed out, no more responses')
-            break
-        else:
-            print ('received {} from {}'.format(data, server))
+	#sent = sock.sendto(message.encode(), multicast_group)
+	while True:
+		read_input,_,_ = select.select([sys.stdin, sock], [sock], [sys.stdin, sock])
+		for notified_input in read_input:
+			if notified_input == sys.stdin:
+				toSend = sys.stdin.readline().strip()
+				sock.sendto(toSend.encode(), multicast_group)
+			if notified_input == sock:
+				mssg, _ = sock.recvfrom(2048)
+				print(mssg.decode())
+        #try:
+        #    data, server = sock.recvfrom(16)
+        #except socket.timeout:
+        #    print('timed out, no more responses')
+        #    break
+        #else:
+        #    print ('received {} from {}'.format(data, server))
 
 finally:
     print('closing socket')
