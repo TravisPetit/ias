2be9cf503655a0868c5e9e6c0b9fe095546bb933
travis <travis@debian-x220>
2020-04-02 21:41:05 +0200

    try adding alternate version for 2.2

--- aufgabe2.2/Client.py
+++ aufgabe2.2/Client.py
@@ -1,5 +1,51 @@
 import socket, select, sys
 
+def wait_for_someone_to_chat_with_me(sock):
+	#sock.bind(("",???)) # or maybe create a new socket
+	print("Okay then, waiting for someone to chat with you ...")
+	partner_uname, partnerAddr = clientSocket.recv(1024) # the partner's first mssg will be their name
+	partner_uname = partner_uname.decode()
+	print("{} wants to chat! Say hi!".format(partner_uname))
+
+	# start chatting
+	try:
+		while True:
+			read_input, _, _ = select.select([sock, sys.stdin], [], [], 1)
+
+			for notified_input in read_input:
+				if notified_input == sys.stdin:
+					toSend = sys.stdin.readline()
+					sock.sendto(toSend.encode(), partnerAddr)
+
+				elif notified_input ==  sock:
+					recieved_message, _ = clientSocket.recv(1024)
+					print("{}: {}".format(partner_uname, recieved_message.decode()))
+	except:
+		print("closing socket ...")
+		sock.close() #always close your sockets!
+
+def start_a_chat_with(sock, partner_name, ip, my_name):
+	changeME = 1234 #??? maybe also create a new socket here
+	sock.sendto(my_name.encode(), (ip, changeME)) # first we send our username
+
+	# then we chat
+	try:
+		while True:
+			read_input, _, _ = select.select([sock, sys.stdin], [], [], 1)
+
+			for notified_input in read_input:
+				if notified_input == sys.stdin:
+					toSend = sys.stdin.readline()
+					sock.sendto(toSend.encode(), partnerAddr)
+
+				elif notified_input ==  sock:
+					recieved_message, _ = clientSocket.recv(1024)
+					print("{}: {}".format(partner_uname, recieved_message.decode()))
+	except Exception as e:
+		print(e)
+		print("closing socket ...")
+		sock.close()
+
 serverIP = "localhost"
 serverPort = 1234
 
@@ -7,42 +53,30 @@ IPv4 = socket.AF_INET
 UDP = socket.SOCK_DGRAM
 clientSocket = socket.socket(IPv4, UDP)
 
-peerIP = ""
-peerPort = ""
-
 username = input("Choose a username: ")
-partnerUsername = input("Enter partner's username: ")
-clientSocket.sendto((username + " " + partnerUsername).encode(), (serverIP, serverPort))
-partnerFound = False
 
-while True:
+clientSocket.sendto(username.encode(), (serverIP, serverPort)) # send name to server
+users_in_server = clientSocket.recv(1024) # the server replies with all the avaliable names from the server
+print(users_in_server.decode())
+partner_index = input("Choose the index of the partner you may want to talk to or reply with NO: ")
 
-    read_input, _, _ = select.select([clientSocket, sys.stdin], [], [], 1)
+if partner_index == "NO":
+	clientSocket.sendto("NO".encode(), (serverIP, serverPort)) # let the server know you are a loner
+	wait_for_someone_to_chat_with_me(clientSocket)
 
-    for notified_input in read_input:
+else:
+	clientSocket.sendto(partner_index.encode(), (serverIP, serverPort)) # send the partner index to the server
+	partner_uname_and_ip, _ = clientSocket.recvfrom(1024) # the server will reply with the partner's name and ip
+	partner_uname_and_ip = partner_uname_and_ip.decode()
 
-        if notified_input == sys.stdin and not partnerFound: #send message to server
-            toSend = sys.stdin.readline()
-            clientSocket.sendto(toSend.encode(), (serverIP, serverPort))
+	# the reply will look something like this ('ben', '127.0.0.1')
+	partner_uname_and_ip = partner_uname_and_ip.split(",")
+	# after the split it looks like this: ('ben'   '127.0.0.1')
 
-        elif notified_input == sys.stdin and partnerFound: #send message to partner
-            toSend = sys.stdin.readline()
-            clientSocket.sendto(toSend.encode(), (peerIP, peerPort))
+	partner_name = partner_uname_and_ip[0] #('ben'
+	ip = partner_uname_and_ip[1] #'127.0.0.1')
 
-        elif notified_input == clientSocket and not partnerFound: #recieve message from server
-            recieved_message = clientSocket.recv(1024)
-            recieved_message = recieved_message.decode()
-            print("Your partner {} wants to chat: ".format(recieved_message))
-            if ("Your partner hasn't connected yet, please wait..." != recieved_message): #your partner is already waiting for you
-                peerInfos = recieved_message.split()
-                peerIP = peerInfos[0] #user saves infos about the partner
-                peerPort = int(peerInfos[1])
-                partnerFound = True
-            # else: the user will be saved in requests list by the server
+	partner_name = partner_name[2:-1] # now instead of ('ben' we have ben
+	ip = ip[2:-2] # same thing with ip
 
-        elif notified_input == clientSocket and partnerFound: #partner already found, msg from partner
-            recieved_message = clientSocket.recv(1024)
-            recieved_message = recieved_message.decode()
-            print("{}: {}".format(partnerUsername, recieved_message))
-
-clientSocket.close()
+	start_a_chat_with(clientSocket, partner_name, ip, username)
--- aufgabe2.2/Server.py
+++ aufgabe2.2/Server.py
@@ -10,41 +10,66 @@ socket = socket.socket(IPv4, UDP)
 socket.bind(("localhost", serverPort))
 print("Server is ready to recieve")
 
-requestsList = []
+#requestsList = []
+users = []
 
 while True:
+	name, addr = socket.recvfrom(1024) # the first mssg from the user to the server will be the user's name
+	print("{} connected to the server!".format(name.decode()))
 
-	read_input, _, _= select.select([socket, sys.stdin], [], [])
+	users.append((name.decode(), addr[0])) #add the username and ip to the userlist
 
-	for notified_input in read_input:
+	s = "### MESSAGE FROM SERVER ###\nThe avaliable users are:\n"
+	for i in range(len(users)):
+		s += "{}: {}\n".format(i, users[i])
+	socket.sendto(s.encode(), addr) # reply with the user list
 
-		if notified_input == socket: #new client
-			found = False
-			userInfos = []
-			userData, userAddress = notified_input.recvfrom(1024)
-			(userIP, userPort) = userAddress
-			userInfos.append(userIP)  # userinfos = [userIP]
-			userInfos.append(userPort)  # connectioninfos = [userIP, userPort]
-			messageList = userData.decode().split()
-			userInfos.append(messageList[0])  # userinfos = [userIP, userPort, own username]
-			userInfos.append(messageList[1])  # userinfos = [userIP, userPort, own username, partner username]
-			# connectionInfos->#0: own IP, 1: own Port, 2: own username, 3: partner username
+	reply, _ = socket.recvfrom(1024) # the user will have replied with either an index or NO
 
-			print("Client Port: ", userPort) #new client connected
+	if reply.decode() == "NO":
+		print("{} will wail for someone to start chatting with them".format(name.decode()))
 
-			for i in requestsList:
-				if userInfos[3] == i[2] and not found:  # partnerusername == (username)already in users
-					# send infos to clients->send ip and port to both
-					socket.sendto((userIP + ' ' + str(userPort)).encode(), (i[0], i[1]))
-					socket.sendto((i[0] + ' ' + str(i[1])).encode(), (userIP, userPort))
-					requestsList.remove(i)
-					#while true!
-					found = True
+	else:
+		mssg = users[int(reply.decode())] # the user at the index that the clinet selected
+		mssg = str(mssg)
+		mssg = mssg.encode()
 
-			#first request
-			if not found:
-				requestsList.append(userInfos)  # saves infos about new client and his peer if request from peer exists
-				socket.sendto(("Your partner hasn't connected yet, please wait...").encode(), (userIP, userPort))
+		socket.sendto(mssg, addr)
+	
 
-
-socket.close()
+#while True:
+#
+#	read_input, _, _= select.select([socket, sys.stdin], [], [])
+#
+#	for notified_input in read_input:
+#
+#		if notified_input == socket: #new client
+#			found = False
+#			userInfos = []
+#			userData, userAddress = notified_input.recvfrom(1024)
+#			(userIP, userPort) = userAddress
+#			userInfos.append(userIP)  # userinfos = [userIP]
+#			userInfos.append(userPort)  # connectioninfos = [userIP, userPort]
+#			messageList = userData.decode().split()
+#			userInfos.append(messageList[0])  # userinfos = [userIP, userPort, own username]
+#			userInfos.append(messageList[1])  # userinfos = [userIP, userPort, own username, partner username]
+#			# connectionInfos->#0: own IP, 1: own Port, 2: own username, 3: partner username
+#
+#			print("Client Port: ", userPort) #new client connected
+#
+#			for i in requestsList:
+#				if userInfos[3] == i[2] and not found:  # partnerusername == (username)already in users
+#					# send infos to clients->send ip and port to both
+#					socket.sendto((userIP + ' ' + str(userPort)).encode(), (i[0], i[1]))
+#					socket.sendto((i[0] + ' ' + str(i[1])).encode(), (userIP, userPort))
+#					requestsList.remove(i)
+#					#while true!
+#					found = True
+#
+#			#first request
+#			if not found:
+#				requestsList.append(userInfos)  # saves infos about new client and his peer if request from peer exists
+#				socket.sendto(("Your partner hasn't connected yet, please wait...").encode(), (userIP, userPort))
+#
+#
+#socket.close()
