3e28c80338a5d86d3fb7367a61a6a525c8f91018
travis <travis@debian-x220>
2020-05-22 01:13:45 +0200

    Add comment mockup

--- s3/a1/ab-encrypt.py
+++ s3/a1/ab-encrypt.py
@@ -2,42 +2,61 @@
 
 import argparse
 from Crypto.Cipher import AES               # requires 'pycrypto' package
-import Crypto.Util
 from Crypto.Util.Padding import pad, unpad  # requires 'pycrypto' package
 
-block_size = 16            # <comment here>
-key = b'!pre-shared-key!'  # <comment here>
+#AES is a block cipher, setting the block size to 16 means that ...
+block_size = 16           
+
+
+# AES is also a symmetric cipher, key contrais the secret key, which is ...
+key = b'!pre-shared-key!'
 
 
 def read_plain(path):
-    file = open(path, "rb")
-    bytes = file.read()
-    file.close()
-    return bytes
+	file = open(path, "rb")
+	bytes = file.read()
+	file.close()
+	return bytes
 
 
 def write_encrypted(bytes, path):
-    file = open(path, "wb")
-    file.write(bytes)
-    file.close()
+	file = open(path, "wb")
+	file.write(bytes)
+	file.close()
 
 
 def main(args):
-    # <comment here>
-    cipher = AES.new(key, AES.MODE_ECB)
 
-    # <comment here>
-    plain_text = read_plain(args.input)
-    padded_plain_text = pad(plain_text, block_size)
+	# we set the mode of operation to ECB, this means that ...
+	cipher = AES.new(key, AES.MODE_ECB)
 
-    # <comment here>
-    cipher_text = cipher.encrypt(padded_plain_text)
-    write_encrypted(cipher_text, args.output)
+	# we read the input bytes from a plaintext file
+	plain_text = read_plain(args.input)
+
+	# then we pad it such that it is fit for 'block_size'
+	# this means that we add some redundant bits at the
+	# end of the file until the number of bytes in the file
+	# is a factor of block size.
+	# so for instance if the block size is 5 bits,
+	# but the message is 12 bits long, we add 3 redundant bits
+	# at the end, that way we can divide the text into
+	# 3 whole blocks when performing AES.
+	# we want to split this in blocks because ...
+	padded_plain_text = pad(plain_text, block_size)
+
+	# we are now ready to encrypt the message
+	# the cipher.encrypt method takes the padded_plain_text
+	# as an input and returns ...
+	cipher_text = cipher.encrypt(padded_plain_text)
+
+	# which is then written to the file whose name is the
+	# value of args.output
+	write_encrypted(cipher_text, args.output)
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Alice-Bob Encryption')
-    parser.add_argument('-i', '--input', type=str, help="Path to plain file", required=True)
-    parser.add_argument('-o', '--output', type=str, help="Path of encrypted file (should not exist)", required=True)
-    args = parser.parse_args()
-    main(args)
+	parser = argparse.ArgumentParser(description='Alice-Bob Encryption')
+	parser.add_argument('-i', '--input', type=str, help="Path to plain file", required=True)
+	parser.add_argument('-o', '--output', type=str, help="Path of encrypted file (should not exist)", required=True)
+	args = parser.parse_args()
+	main(args)
